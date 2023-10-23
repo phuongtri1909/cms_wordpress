8e4b7c1b636afa271affc41b8ed4b07c11c7d32a
vunguqua10 <vunguqua10@gmail.com>
2023-10-24 03:14:10 +0700

    Module 10

--- wp-content/themes/twentytwentyone/template-parts/content/content-single.php
+++ wp-content/themes/twentytwentyone/template-parts/content/content-single.php
@@ -10,35 +10,51 @@
  */
 
 ?>
+<div class="container-detail">
+    <div class="row">
+        <div class="col-md-3">
+            <!-- Nội dung các cột khác trong col-md-2 ở đây -->
+        </div>
+        <div class="col-md-6">
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+            </header>
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+        </article>
+        </div>
+        <div class="col-md-3">
+            <div class="recent-posts-container post-content-bg">
+                <?php
+                $recent_posts = get_posts(array(
+                    'post_type' => 'post',
+                    'posts_per_page' => 3,
+                ));
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                foreach ($recent_posts as $post) :
+                    setup_postdata($post);
+                    $day = get_the_date('d');
+                    $month = get_the_date('m');
+                    $year = substr(get_the_date('Y'), -2, 2);
+                    ?>
+                    <article id="post-<?php the_ID(); ?>" <?php post_class('post-content-recent'); ?>>
+                        <div class="post-content-date">
+                            <div class="post-md">
+                                <span class="post-day post-color"><?php echo $day; ?></span>
+                                <span class="post-month post-color"><?php echo $month; ?></span>
+                            </div>
+                            <span class="post-year post-color"><?php echo $year; ?></span>
+                            <?php get_template_part('template-parts/header/excerpt-header', get_post_format()); ?>
+                        </div>
 
-	<header class="entry-header alignwide">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		<?php twenty_twenty_one_post_thumbnail(); ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-		<?php
-		the_content();
-
-		wp_link_pages(
-			array(
-				'before'   => '<nav class="page-links" aria-label="' . esc_attr__( 'Page', 'twentytwentyone' ) . '">',
-				'after'    => '</nav>',
-				/* translators: %: Page number. */
-				'pagelink' => esc_html__( 'Page %', 'twentytwentyone' ),
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer default-max-width">
-		<?php twenty_twenty_one_entry_meta_footer(); ?>
-	</footer><!-- .entry-footer -->
-
-	<?php if ( ! is_singular( 'attachment' ) ) : ?>
-		<?php get_template_part( 'template-parts/post/author-bio' ); ?>
-	<?php endif; ?>
-
-</article><!-- #post-<?php the_ID(); ?> -->
+                    </article><!-- #post-${ID} -->
+                <?php
+                endforeach;
+                wp_reset_postdata();
+                ?>
+            </div>
+        </div>
+    </div>
+</div>

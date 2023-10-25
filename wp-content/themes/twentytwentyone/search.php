0f8115ee934acc3e21f363ad18dfbee82f6f1d2a
vunguqua10 <vunguqua10@gmail.com>
2023-10-25 13:30:49 +0700

    fix search

--- wp-content/themes/twentytwentyone/search.php
+++ wp-content/themes/twentytwentyone/search.php
@@ -22,35 +22,6 @@ get_header();
 
 	</div>
 	<div class="col-md-6">
-		<?php if ( have_posts() ) {?>
-			<header class="page-header alignwide">
-				<h1 class="page-title">
-					<?php
-						printf(
-							/* translators: %s: Search term. */
-							esc_html__( 'Results for "%s"', 'twentytwentyone' ),
-							'<span class="page-description search-term">' . esc_html( get_search_query() ) . '</span>'
-						);
-					?>
-				</h1>
-			</header><!-- .page-header -->
-
-			<div class="search-result-count default-max-width">
-				<?php
-					printf(
-						esc_html(
-							/* translators: %d: The number of search results. */
-							_n(
-								'We found %d result for your search.',
-								'We found %d results for your search.',
-								(int) $wp_query->found_posts,
-								'twentytwentyone'
-							)
-						),
-						(int) $wp_query->found_posts
-					);
-				?>
-			</div><!-- .search-result-count -->
 			<?php
 					// Start the Loop.
 					while ( have_posts() ) {
@@ -91,7 +62,7 @@ get_header();
 				twenty_twenty_one_the_posts_navigation();
 
 				// If no content, include the "No posts found" template.
-		}?>
+		?>
 	</div>
 
 	<div class="col-md-3">

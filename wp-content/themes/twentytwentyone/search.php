2a3d1021fafbf0a6b6f4f0a429cce0aa503e621f
DangVanTrong <91874945+DangVanTrong@users.noreply.github.com>
2023-10-30 21:22:24 +0700

    lastpostsss

--- wp-content/themes/twentytwentyone/search.php
+++ wp-content/themes/twentytwentyone/search.php
@@ -99,7 +99,7 @@ get_header();
 </div>
 <div class="row">
 		<div class="container mt-5 mb-5">
-			<div class="row">
+			<div class="row br">
 				<div class="col-md-6 offset-md-3">
 					<h4 class="latest-news-title">Latest News</h4>
 						<ul class="timeline lastpost">
